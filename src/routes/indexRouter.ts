7d34595ec98252e00a13f7ab67ba0992a3eb75db
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-05-08 19:18:35 +0100

    Added conversation routes

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,14 +1,12 @@
 import { Request, Response, Router } from "express";
 import authRoutes from "./authRoutes";
 import profileRoutes from "./profileRoutes";
+import conversationRoutes from "./conversationRoutes";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response): void => {
-  res.json("api");
-});
-
 router.use("/auth", authRoutes);
 router.use("/profiles", profileRoutes);
+router.use("/conversations", conversationRoutes);
 
 export default router;
